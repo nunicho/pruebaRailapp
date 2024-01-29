7db0ee12765ea1e8a64f572b2a8eae195ebe7446
MauricioAlonso <program.alonso@gmail.com>
2024-01-29 18:42:23 -0300

    avanzado en test

--- test/supertest-Carts.test.js
+++ test/supertest-Carts.test.js
@@ -15,39 +15,110 @@ async function runTests() {
     this.timeout(6000);
 
     describe("Pruebas al módulo carritos", function () {
-
-      it("SUPERTEST: El endpoint /api/carts, con método POST, permite crear un carrito nuevo en BD. Además se genera también un ticket mediante ticket.controller", async function () {
-      const productosEnCarrito = [
-        {
-          id: config.PRODUCTO_TEST_EJEMPLO,
-          quantity:2,
-        },
-      ];
-      
-      const carrito = { products: productosEnCarrito };
-      try {
-        const { body, ok, statusCode } = await requester
-          .post("/api/carts/purchase")
-          .send(carrito);
-
-        expect(statusCode).to.equal(201);
-        expect(ok).to.be.true;
-        expect(body).to.have.property("carritoInsertado").that.is.an("object");
-        expect(body.carritoInsertado).to.have.property("_id");
-      } catch (error) {
-        console.error("Test Error:", error);
-        throw error; 
-      }
-    });
-
-      it("SUPERTEST: El endpoint /api/carritos, con método GET, obtiene carritos de la Base de datos", async function () {
+      it("SUPERTEST: VER TODOS LOS CARRITOS: El endpoint /api/carts, con método GET, obtiene carritos de la Base de datos", async function () {
         let { body, ok, statusCode } = await requester.get("/api/carts");
 
         expect(statusCode).to.equal(200);
-        expect(ok).to.be.true;  
+        expect(ok).to.be.true;
         expect(body).to.have.property("data").that.is.an("array");
       });
 
+      it("SUPERTEST: VER CARRITO: El endpoint /api/carts/:cid, con método GET, obtiene un carrito por ID", async function () {
+        const carritoId = config.CART_TEST_EJEMPLO;
+
+        const { body, ok, statusCode } = await requester.get(
+          `/api/carts/${carritoId}`
+        );
+
+        expect(statusCode).to.equal(200);
+        expect(ok).to.be.true;
+        expect(body).to.have.property("_id").that.is.equal(carritoId);
+        expect(body).to.have.property("productos").that.is.an("array");
+      });
+
+      it("SUPERTEST: AGREGAR PRODUCTO: El endpoint /api/carts/:id/agregarProducto, con método POST, agrega productos al carrito", async function () {
+        const userId = config.USER_TEST_EJEMPLO;
+        const products = [
+          { productoId: config.PRODUCTO_TEST_EJEMPLO, quantity: 2 },
+        ];
+
+        const { body, ok, statusCode } = await requester
+          .post(`/api/carts/${userId}/agregarProducto`)
+          .send({ products });
+
+        expect(statusCode).to.equal(200);
+        expect(ok).to.be.true;
+        expect(body)
+          .to.have.property("mensaje")
+          .that.equals("Productos agregados al carrito con éxito");
+      });
+
+      it("SUPERTEST: QUITAR PRODUCTO: El endpoint /api/carts/:id/quitarProducto, con método POST, quita un producto del carrito", async function () {
+        const userId = config.USER_TEST_EJEMPLO;
+        const productId = config.PRODUCTO_TEST_EJEMPLO;
+        const { body, ok, statusCode } = await requester
+          .post(`/api/carts/${userId}/quitarProducto`)
+          .send({ productId });
+
+        expect(statusCode).to.equal(200);
+        expect(ok).to.be.true;
+        expect(body)
+          .to.have.property("mensaje")
+          .that.equals("Producto quitado del carrito con éxito");
+      });
+
+      it("SUPERTEST: LIMPIAR CARRITO: El endpoint /api/carts/:id/agregarProducto, con método POST, agrega productos al carrito y luego limpia el carrito", async function () {
+        const userId = config.USER_TEST_EJEMPLO;
+        const products = [
+          { productoId: config.PRODUCTO_TEST_EJEMPLO, quantity: 2 },
+        ];
+
+        const agregarProductoResponse = await requester
+          .post(`/api/carts/${userId}/agregarProducto`)
+          .send({ products });
+
+        expect(agregarProductoResponse.statusCode).to.equal(200);
+        expect(agregarProductoResponse.ok).to.be.true;
+        expect(agregarProductoResponse.body)
+          .to.have.property("mensaje")
+          .that.equals("Productos agregados al carrito con éxito");
+
+        const limpiarCarritoResponse = await requester.post(
+          `/api/carts/${userId}/limpiarCarrito`
+        );
+        expect(limpiarCarritoResponse.statusCode).to.equal(200);
+        expect(limpiarCarritoResponse.ok).to.be.true;
+        expect(limpiarCarritoResponse.body)
+          .to.have.property("mensaje")
+          .that.equals("Carrito limpiado con éxito");
+      });
+
+      it("SUPERTEST: REALIZAR COMPRA: El endpoint /api/carts/:id/agregarProducto, con método POST, agrega productos al carrito y luego realiza la compra", async function () {
+        const userId = config.USER_TEST_EJEMPLO;
+        const products = [
+          { productoId: config.PRODUCTO_TEST_EJEMPLO, quantity: 2 },
+        ];
+
+        const agregarProductoResponse = await requester
+          .post(`/api/carts/${userId}/agregarProducto`)
+          .send({ products });
+
+        expect(agregarProductoResponse.statusCode).to.equal(200);
+        expect(agregarProductoResponse.ok).to.be.true;
+        expect(agregarProductoResponse.body)
+          .to.have.property("mensaje")
+          .that.equals("Productos agregados al carrito con éxito");
+
+        const realizarCompraResponse = await requester.post(
+          `/api/carts/${userId}/purchase`
+        );
+
+        expect(realizarCompraResponse.statusCode).to.equal(200);
+        expect(realizarCompraResponse.ok).to.be.true;
+        expect(realizarCompraResponse.body)
+          .to.have.property("mensaje")
+          .that.equals("Compra realizada con éxito, se generó el ticket");
+      });
     });
   });
 }
